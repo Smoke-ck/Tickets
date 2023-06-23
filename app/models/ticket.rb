af7532b15fa5852d5d7ba19fecab80e16a922152
smoke-ck <2056sanya9snake@gmail.com>
2023-06-23 07:12:30 +0300

    Make code clear

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,17 @@
 class Ticket < ApplicationRecord
   has_one :excavator, dependent: :destroy
-  has_one :excavation_info, through: :excavator
 
-  accepts_nested_attributes_for :excavator
+  delegate :company_name, :address, :crew_onsite, to: :excavator, allow_nil: true, prefix: true
+
+  def date_time
+    date_times["response_due_date_time"]
+  end
+
+  def primary_service_area_code
+    service_area["primary_service_area_code"]["sa_code"]
+  end
+
+  def additional_service_area_codes
+    service_area["additional_service_area_codes"]["sa_code"]
+  end
 end
